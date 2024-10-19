632d74d39fd981a9d5b1a50d3e53e6a32229363e
neil.pan <neil.pan@qq.com>
2024-10-19 22:43:19 +0800

    :)

---
+++ sw.js
@@ -0,0 +1,366 @@
+/**
+ * Copyright 2018 Google Inc. All Rights Reserved.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+// If the loader is already loaded, just stop.
+if (!self.define) {
+  let registry = {};
+
+  // Used for `eval` and `importScripts` where we can't get script URL by other means.
+  // In both cases, it's safe to use a global var because those functions are synchronous.
+  let nextDefineUri;
+
+  const singleRequire = (uri, parentUri) => {
+    uri = new URL(uri + ".js", parentUri).href;
+    return registry[uri] || (
+      
+        new Promise(resolve => {
+          if ("document" in self) {
+            const script = document.createElement("script");
+            script.src = uri;
+            script.onload = resolve;
+            document.head.appendChild(script);
+          } else {
+            nextDefineUri = uri;
+            importScripts(uri);
+            resolve();
+          }
+        })
+      
+      .then(() => {
+        let promise = registry[uri];
+        if (!promise) {
+          throw new Error(`Module ${uri} didn’t register its module`);
+        }
+        return promise;
+      })
+    );
+  };
+
+  self.define = (depsNames, factory) => {
+    const uri = nextDefineUri || ("document" in self ? document.currentScript.src : "") || location.href;
+    if (registry[uri]) {
+      // Module is already loading or loaded.
+      return;
+    }
+    let exports = {};
+    const require = depUri => singleRequire(depUri, uri);
+    const specialDeps = {
+      module: { uri },
+      exports,
+      require
+    };
+    registry[uri] = Promise.all(depsNames.map(
+      depName => specialDeps[depName] || require(depName)
+    )).then(deps => {
+      factory(...deps);
+      return exports;
+    });
+  };
+}
+define(['./workbox-9637eeee'], (function (workbox) { 'use strict';
+
+  self.addEventListener('message', event => {
+    if (event.data && event.data.type === 'SKIP_WAITING') {
+      self.skipWaiting();
+    }
+  });
+
+  /**
+   * The precacheAndRoute() method efficiently caches and responds to
+   * requests for URLs in the manifest.
+   * See https://goo.gl/S9QRab
+   */
+  workbox.precacheAndRoute([{
+    "url": "assets/404-COw5hksb.png",
+    "revision": null
+  }, {
+    "url": "assets/7sx6j6O9.css",
+    "revision": null
+  }, {
+    "url": "assets/ahbhI-1g.js",
+    "revision": null
+  }, {
+    "url": "assets/app-BFJbXYaq.js",
+    "revision": null
+  }, {
+    "url": "assets/AUb8BxGU.js",
+    "revision": null
+  }, {
+    "url": "assets/B8SDKQpV.js",
+    "revision": null
+  }, {
+    "url": "assets/B9k3MDNK.js",
+    "revision": null
+  }, {
+    "url": "assets/BCeqVwJ2.css",
+    "revision": null
+  }, {
+    "url": "assets/BCL5yW7Q.js",
+    "revision": null
+  }, {
+    "url": "assets/BjnNLqVx.js",
+    "revision": null
+  }, {
+    "url": "assets/Bl0zWvo-.css",
+    "revision": null
+  }, {
+    "url": "assets/BquGyE27.js",
+    "revision": null
+  }, {
+    "url": "assets/BRoWSGHI.css",
+    "revision": null
+  }, {
+    "url": "assets/BRSnYHbg.css",
+    "revision": null
+  }, {
+    "url": "assets/BWEhohXz.js",
+    "revision": null
+  }, {
+    "url": "assets/BymVHglh.js",
+    "revision": null
+  }, {
+    "url": "assets/C-UO50Uj.js",
+    "revision": null
+  }, {
+    "url": "assets/C39qY3MF.css",
+    "revision": null
+  }, {
+    "url": "assets/C3OJiXsV.js",
+    "revision": null
+  }, {
+    "url": "assets/C6rnkjJU.js",
+    "revision": null
+  }, {
+    "url": "assets/CfgA1tQO.js",
+    "revision": null
+  }, {
+    "url": "assets/CJWvpnsb.css",
+    "revision": null
+  }, {
+    "url": "assets/CP3iFwBu.js",
+    "revision": null
+  }, {
+    "url": "assets/CqzHAeJK.js",
+    "revision": null
+  }, {
+    "url": "assets/CrmVL1Tk.css",
+    "revision": null
+  }, {
+    "url": "assets/CUA4hNHl.js",
+    "revision": null
+  }, {
+    "url": "assets/cvAtn-sq.js",
+    "revision": null
+  }, {
+    "url": "assets/CvywAfwY.js",
+    "revision": null
+  }, {
+    "url": "assets/CxBnUIjN.js",
+    "revision": null
+  }, {
+    "url": "assets/D10w8BdT.js",
+    "revision": null
+  }, {
+    "url": "assets/DAFdu9Q-.js",
+    "revision": null
+  }, {
+    "url": "assets/DawlIFS4.js",
+    "revision": null
+  }, {
+    "url": "assets/DCBIG-s4.js",
+    "revision": null
+  }, {
+    "url": "assets/DCr4mGtC.js",
+    "revision": null
+  }, {
+    "url": "assets/DDO2Ugl1.css",
+    "revision": null
+  }, {
+    "url": "assets/Dfd81F6s.js",
+    "revision": null
+  }, {
+    "url": "assets/DJgmcNpC.js",
+    "revision": null
+  }, {
+    "url": "assets/DQ5Zupmz.js",
+    "revision": null
+  }, {
+    "url": "assets/DQicnYhK.js",
+    "revision": null
+  }, {
+    "url": "assets/DtfnXpzP.js",
+    "revision": null
+  }, {
+    "url": "assets/Dx3U6qo9.js",
+    "revision": null
+  }, {
+    "url": "assets/DZWGFCVr.js",
+    "revision": null
+  }, {
+    "url": "assets/header-mountains-desktop-CVBy9rgG.svg",
+    "revision": null
+  }, {
+    "url": "assets/header-mountains-mobile-G1eVZBnf.svg",
+    "revision": null
+  }, {
+    "url": "assets/HG7Y41XE.js",
+    "revision": null
+  }, {
+    "url": "assets/HX3MPmxt.js",
+    "revision": null
+  }, {
+    "url": "assets/iEwDEPZd.js",
+    "revision": null
+  }, {
+    "url": "assets/jJ6pMX5Y.js",
+    "revision": null
+  }, {
+    "url": "assets/logo-DG9Mi4BC.svg",
+    "revision": null
+  }, {
+    "url": "assets/model-CFsaf2GQ.jpg",
+    "revision": null
+  }, {
+    "url": "assets/nFXwx1WQ.js",
+    "revision": null
+  }, {
+    "url": "assets/NlRR9t1G.js",
+    "revision": null
+  }, {
+    "url": "assets/nXmhecTI.js",
+    "revision": null
+  }, {
+    "url": "assets/people-CCsKVy4-.svg",
+    "revision": null
+  }, {
+    "url": "assets/pIwD7FTM.js",
+    "revision": null
+  }, {
+    "url": "assets/Ql8p2Dk4.js",
+    "revision": null
+  }, {
+    "url": "assets/RTKFCJmK.js",
+    "revision": null
+  }, {
+    "url": "assets/upgrade-CkrSpVnk.svg",
+    "revision": null
+  }, {
+    "url": "assets/VqVODLTx.js",
+    "revision": null
+  }, {
+    "url": "assets/wx-Bfv1OeoY.png",
+    "revision": null
+  }, {
+    "url": "icons/apple-touch-icon-180x180.png",
+    "revision": "afec7503d21ce05189d43faedf461c5f"
+  }, {
+    "url": "icons/apple-touch-icon.png",
+    "revision": "61dea1c20742b36e7d7c027ec8316e7a"
+  }, {
+    "url": "icons/favicon.ico",
+    "revision": "be6e5c5ca64497226387bc628a5ed0d3"
+  }, {
+    "url": "icons/icon.svg",
+    "revision": "13cea5a25a7eaee5ee3366d6ad6e9955"
+  }, {
+    "url": "icons/maskable-icon-512x512.png",
+    "revision": "bd537be15c94897fdcb14b5ce0acbcff"
+  }, {
+    "url": "icons/pwa-192x192.png",
+    "revision": "6479efa2375828a17a7e00e2cee453af"
+  }, {
+    "url": "icons/pwa-512x512.png",
+    "revision": "6bf172e452d788c5053fc8afd0f53833"
+  }, {
+    "url": "icons/pwa-64x64.png",
+    "revision": "bdcfe23999a1d5f998517bf82efa2e52"
+  }, {
+    "url": "404.html",
+    "revision": "cc3d5acfbc16b4d1b5dd8a6f9413f386"
+  }, {
+    "url": "enfj.html",
+    "revision": "1d1aa69630b160787b56149ac4eaeb6b"
+  }, {
+    "url": "enfp.html",
+    "revision": "8ee3d2a832721b3b9a45e80b288be2a6"
+  }, {
+    "url": "entj.html",
+    "revision": "33134ca061b0a25399300d5fe8f44e9c"
+  }, {
+    "url": "entp.html",
+    "revision": "724bad7bd3d584c0f869c37a2789d292"
+  }, {
+    "url": "esfj.html",
+    "revision": "6574e8ca4ee648daf7cb071350cd1e18"
+  }, {
+    "url": "esfp.html",
+    "revision": "076e441fdfe6a3b6c7015fbbd69cf45a"
+  }, {
+    "url": "estj.html",
+    "revision": "f327fa8bea6fd1a4d8e79592066074ce"
+  }, {
+    "url": "estp.html",
+    "revision": "1ee6bf65394df8116d386d4259739567"
+  }, {
+    "url": "index.html",
+    "revision": "77382465f0d8acc3d4e8ab87026edfd7"
+  }, {
+    "url": "infj.html",
+    "revision": "50fef317fa01da32e527c475769cf9a0"
+  }, {
+    "url": "infp.html",
+    "revision": "0092e3525adc9419894aed12bb3e5815"
+  }, {
+    "url": "intj.html",
+    "revision": "cc2e19953ab0d9f2cd8756f78a3be714"
+  }, {
+    "url": "intp.html",
+    "revision": "f773d3249cfd21ad797e87c0c046e711"
+  }, {
+    "url": "isfj.html",
+    "revision": "891a118ece2a03bbd45fdbb4d07d37b4"
+  }, {
+    "url": "isfp.html",
+    "revision": "17b45a1098bc362ea56af3235bcb43b2"
+  }, {
+    "url": "istj.html",
+    "revision": "5c2ad80dbe8b7b584f6987ba082837ed"
+  }, {
+    "url": "istp.html",
+    "revision": "858e9c8937f0dd025f4406122f34a610"
+  }, {
+    "url": "registerSW.js",
+    "revision": "1872c500de691dce40960bb85481de07"
+  }, {
+    "url": "result.html",
+    "revision": "b28129a5e7f686fe9bcfb8baa017b243"
+  }, {
+    "url": "test.html",
+    "revision": "f593b745f9cc214d3b6202cf51ed7943"
+  }, {
+    "url": "types.html",
+    "revision": "f9c42a4b333b200bab64456bdd3ed6e9"
+  }, {
+    "url": "icons/pwa-192x192.png",
+    "revision": "6479efa2375828a17a7e00e2cee453af"
+  }, {
+    "url": "icons/pwa-512x512.png",
+    "revision": "6bf172e452d788c5053fc8afd0f53833"
+  }, {
+    "url": "manifest.webmanifest",
+    "revision": "b2ced5a6cd4884bef3bcd99a3b3c171a"
+  }], {});
+  workbox.cleanupOutdatedCaches();
+  workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html")));
+
+}));
